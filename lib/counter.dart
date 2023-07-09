6ded4372cc8755f19efbb656bfa96de5097ca89c
Abdul Rahim <113752743+nextive-rahim@users.noreply.github.com>
2023-07-09 11:44:51 +0600

    keep app background

---
+++ lib/counter.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/foundation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Counter {
+  Counter() {
+    _readCount().then((count) => _count.value = count);
+  }
+
+  ValueNotifier<int> _count = ValueNotifier(0);
+
+  ValueListenable<int> get count => _count;
+
+  void increment() {
+    _count.value++;
+    _writeCount(_count.value);
+  }
+
+  Future<int> _readCount() async {
+    var prefs = await SharedPreferences.getInstance();
+    return prefs.getInt('Counter.count') ?? 0;
+  }
+
+  Future<bool> _writeCount(int count) async {
+    var prefs = await SharedPreferences.getInstance();
+    return prefs.setInt('Counter.count', count);
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,98 @@
+import 'dart:async';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_background/app_retain_widget.dart';
+import 'package:flutter_background/background_main.dart';
+import 'package:flutter_background/counter_service.dart';
+
+void main() {
+  runApp(MyApp());
+
+  var channel = const MethodChannel('com.example/background_service');
+  var callbackHandle = PluginUtilities.getCallbackHandle(backgroundMain);
+  channel.invokeMethod('startService', callbackHandle!.toRawHandle());
+
+  CounterService.instance().startCounting();
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Background Demo',
+      home: AppRetainWidget(
+        child: MyWidget(),
+      ),
+    );
+  }
+}
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  int _counter = 0;
+  @override
+  void initState() {
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        _counter++;
+      });
+      print('Counter incrementeds: $_counter');
+    });
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Background Processing'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text(
+          _counter.toString(),
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Background Demo'),
+      ),
+      body: Center(
+        child: ValueListenableBuilder(
+          valueListenable: CounterService.instance().count,
+          builder: (context, count, child) {
+            return Text('Counting: $count');
+          },
+        ),
+      ),
+    );
+  }
+}
